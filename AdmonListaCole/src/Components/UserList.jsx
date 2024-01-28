33f2270ec5db01cba6bacf6d8131cf64f679eda2
Analucena53 <lucenapuertasana53@gmail.com>
2024-01-28 23:09:30 +0100

    cuerpo del html en proceso

--- AdmonListaCole/src/Components/UserList.jsx
+++ AdmonListaCole/src/Components/UserList.jsx
@@ -1,3 +1,5 @@
+import logoCole from './images/logoCole.png';
+import conf from './images/conf.svg';
 
 //para usar los hooks hay que importarlos
 import { useState, } from "react";
@@ -10,13 +12,22 @@ const UserList = () => {
         nameList: '',
         name: '',
         lastName: '',
+        lastName2:'',
+        email:'',
+        tlf:''
     });
     const [userList, setUserList] = useState([]);
-    const [listTitle, setListTitle] = useState('');
+    
     //nuevo estado para el titulo
+    const [listTitle, setListTitle] = useState('');
 
     //esta función coge el valor de "user" usando "setUser" y le asigna el valor del input, siendo el evento que dispara esta función
     //cualquier cambio que un usuario haga en el input al editarlo
+
+    function handlelistNameChange(e) {
+        setUser({ ...user, nameList: e.target.value });
+    };
+
     function handleNameChange(e) {
         setUser({ ...user, name: e.target.value });
     };
@@ -25,28 +36,35 @@ const UserList = () => {
         setUser({ ...user, lastName: e.target.value });
     };
 
-    function handlelistNameChange(e) {
-        setUser({ ...user, nameList: e.target.value });
+    function handlelastName2Change(e) {
+        setUser({ ...user, lastName2: e.target.value });
     };
 
+    function handleemailChange(e) {
+        setUser({...user, email: e.target.value })
+    };
+
+    function handletlfChange(e) {
+        setUser({...user, tlf: e.target.value })
+    }
+
+    
     //a tráves del evento de click (onclick) del botón se ejecuta esta función, que guarda en un array la variable user mediante el
     //"setUserList"
     function handleAddUserToList() {
         setUserList(prevUserList => [...prevUserList, {...user}]);// Crear un nuevo objeto para user
+        
+    //Establecer el título con el valor de nameList
         setListTitle(user.nameList);
-        //Establecer el título con el valor de nameList
 
-        //este console log lo pusimos por razones ajenas al objetivo de la aplicación, queríamos comprobar que el array se fuera cargando sin 
-        //más y también lo pusimos para tener un punto de parada en los breakpoint. Sin embargo, el array no aparecía actualizado. El problema
-        //era que si está metido dentro de la funcióno nunca iba a imprimir el array actualizado. No nos hemos dado cuenta y pensábamos
-        //que había errores en el código. Unas risas.
 
-        //   console.log(userList);
-
-        //Limpiar el formulario
+    //Limpiar el formulario
         setUser({
             name: '',
             lastName: '',
+            lastName2:'',
+            email:'',
+            tlf:''
         });
 
     }
@@ -56,53 +74,65 @@ const UserList = () => {
     console.log(userList);
 
 
-
-    //Creíamos que la solución al array que no se acutalizaba era usar useEffect. En realidad no era necesario, pero en cualquier caso
-    //hemos aprendido a usarlo: sirve para manipular efectos secundarios, por ejemplo, para asegurarte de que lo que declares dentro del 
-    //useEffect no se ejecute hasta que se haya actualizado el estado del componenente o de una variable, que es lo que ponemos dentro
-    //del corchete (se llama dependencia). El console no se ejecutaría hasta que se hubiera actualizado el estado (valor) de userList
-
-    //   useEffect(()=>{
-    //     console.log(userList)
-    //   },[userList,])
-
-
     // en el return usamos un .map() en el array userList para transformar sus elementos. .map() itera sobre cada elemento, lo transforma 
     //y devuelve un array nuevo con los elementos transformados. Aquí lo usamos para asignar a cada elemento un <li> con su contenido 
     // (que es un nombre, un string)
     return (
         <>
-            <h1>Qué lista eres</h1>
+
+        <nav class="contentNav">
+            <img class="logo" src={logoCole} alt="logo colegio Arrabal"/>
+            <button class="buttonnav" onclick="addList()">+Lista</button>
+            <button class="buttonnav" onclick="editList()">Editar</button>
+            <button class="buttonnav" onclick="delet()">Eliminar</button>
+            <img class="conf" src={conf} alt="configuración"></img>
+        </nav>
+
             <label>
-                <h2>Evento</h2>
+                <h2>Lista:</h2>
                 <input type="text" id="textUserlistName" name="ListName" value={user.nameList} onChange={handlelistNameChange} />
             </label>
 
+            <form class="formList">
             <label>
                 <h2>Nombre</h2>
                 <input type="text" id="textUserName" name="userName" value={user.name} onChange={handleNameChange} />
             </label>
 
             <label>
-                <h2>Apellido</h2>
+                <h2>Apellido1</h2>
                 <input type="text" id="textUserlastName" name="userLastname" value={user.lastName} onChange={handlelastNameChange} />
+            </label>
+            <label>
+                <h2>Apellido2</h2>
+                <input type="text" id="textUserlastName2" name="userLastname2" value={user.lastName2} onChange={handlelastName2Change} />
+            </label>
+            <label>
+                <h2>Email</h2>
+                <input type="text" id="textUserEmail" name="userEmail" value={user.email} onChange={handleemailChange} />
+            </label>
+            <label>
+                <h2>Teléfono</h2>
+                <input type="text" id="textUserTlf" name="userTlf" value={user.tlf} onChange={handletlfChange} />
             </label>
 
 
-            <button onClick={handleAddUserToList}>Añadir usuario</button>
+            <button class="buttonForm" onClick={handleAddUserToList}>Enviar</button>
+            </form>
 
             {listTitle && (
                 <>
                     <h2>{`Evento: ${listTitle}`}</h2>
                     <ol>
                         {userList.map((user, index) => (
-                            <li key={index}>
-                                {user.name} {user.lastName}
-                            </li>
+                        <li key={index}>
+                            {user.name} {user.lastName} {user.lastName2} {user.email} {user.tlf}
+                        </li>
                         ))}
                     </ol>
                 </>
             )}
+            
         </>
 
     )
